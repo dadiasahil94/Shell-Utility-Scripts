52c3fd634155387950f581df4c563288a286e12b
Sahil Dadia <dadiasahil94@yahoo.in>
2016-05-05 14:52:33 +0530

    Added the file to uninstall everything

---
+++ uninstall.sh
@@ -0,0 +1,16 @@
+find . -type f -iname "*.sh" -printf "%f\n" | sed 's/\.[^.]*$//' > record.txt 
+
+no_lines=$(wc ./record.txt -l)
+current=$0
+
+while true;
+do
+	line=$(head -n 1 ./record.txt)
+	echo $line
+#	echo $current
+	find /usr/local/bin/  -type f -iname "$line.sh" -exec rm -rf {} \;
+	find /usr/bin/  -type f -iname "$line"  -exec rm -rf {} \;
+#	current=$[$current+1]
+	sed -i '1d' ./record.txt
+done
+
